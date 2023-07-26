d4fb80224fdcf3eea9331a27166da14a1357df76
ysh <dbtmdgks7897@outlook.kr>
2023-07-26 11:28:15 +0900

    에러처리

--- src/main/java/com/example/my/common/exception/handler/RestExceptionHandler.java
+++ src/main/java/com/example/my/common/exception/handler/RestExceptionHandler.java
@@ -10,9 +10,10 @@ import com.example.my.common.exception.BadRequestException;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
-
+    // 에러별로 만들기 (추천)
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<?> handleBadRequestException(Exception exception){
+        exception.printStackTrace();
         return new ResponseEntity<>(
         ResponseDTO.builder()
                 .code(1)
@@ -20,6 +21,18 @@ public class RestExceptionHandler {
                 .build(),
             HttpStatus.BAD_REQUEST);    
         
+    }
+
+    //모든 에러를 처리하고 싶다
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception exception){
+        exception.printStackTrace();  // 함수들이 실행되는 것을 수정한다. (어디서 에러가 발생했는지 추적)
+        return new ResponseEntity<>(
+            ResponseDTO.builder()
+                .code(1)
+                .message(exception.getMessage())
+                .build(),
+            HttpStatus.INTERNAL_SERVER_ERROR);
     }
     
 }
