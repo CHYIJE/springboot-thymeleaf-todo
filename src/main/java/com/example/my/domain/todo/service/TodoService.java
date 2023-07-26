8b422dddcf451a509ac562f8046a576d2df5c7b3
ysh <dbtmdgks7897@outlook.kr>
2023-07-26 17:50:26 +0900

    할일 리스트

--- src/main/java/com/example/my/domain/todo/service/TodoService.java
+++ src/main/java/com/example/my/domain/todo/service/TodoService.java
@@ -1,10 +1,13 @@
 package com.example.my.domain.todo.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.my.common.dto.LoginUserDTO;
 import com.example.my.domain.todo.dto.ResTodoTableDTO;
+import com.example.my.model.todo.entity.TodoEntity;
 import com.example.my.model.todo.repository.TodoRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,7 +21,11 @@ public class TodoService {
 
     public ResTodoTableDTO getTodoTableData(LoginUserDTO loginUserDTO) {
         // TODO : 리파지토리에서 유저 기본키로 삭제되지 않은 할 일 목록 찾기
+        List<TodoEntity> todoEntityList = todoRepository.findByUserEntity_IdxAndDeleteDateIsNull(loginUserDTO.getUser().getIdx());
+
         
+
+
         return null;
     }
 
